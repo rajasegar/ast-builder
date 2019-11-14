954f166c0be7690b76a609a3de72a1b359160f72
Rajasegar Chandran <rajasegar.c@gmail.com>
2019-11-14 15:29:33 +0530

    [FEAT] Handling block statements for hbs

--- app/components/ast-builder-hbs.js
+++ app/components/ast-builder-hbs.js
@@ -2,16 +2,31 @@ import Component from '@ember/component';
 import { computed } from '@ember/object';
 import { parse, print, builders } from 'ember-template-recast';
 import { inject as service } from '@ember/service';
-import glimmer from '../utils/template-recast-builders';
+import etr from '../utils/template-recast-builders';
 const b = builders; // eslint-disable-line
 
-const _code = `hello
-<div class="entry">
-  <h1>{{title}}</h1>
-  <div class="body">
-    {{body}}
-  </div>
-</div>`;
+let _code = `<BsButton @onClick={{action "submit"}}>
+Button
+  </BsButton>
+
+<BsButtonGroup @value={{buttonGroupValue}} @type="checkbox" @onChange={{action (mut buttonGroupValue)}} as |bg|>
+          <bg.button @value={{1}}>1</bg.button>
+          <bg.button @value={{2}}>2</bg.button>
+          <bg.button @value={{3}}>3</bg.button>
+        </BsButtonGroup>
+ <MyComponent @prop1={{true}} @prop2={{false}} />
+ <XFoo @data-foo={{true}} />
+        <XFoo @data-test-selector={{true}} />
+        <XFoo @data-test-selector={{post.id}} />
+        <XFoo @label="hi" @data-test-selector={{true}} />
+  `;
+
+_code = `
+{{#common/accordion-component data-test-accordion as |accordion|}}
+          block
+        {{/common/accordion-component}}
+`;
+
 export default Component.extend({
   customize: service(),
   code: _code,
@@ -24,22 +39,24 @@ export default Component.extend({
   }),
 
   pseudoAst: computed('code', function() {
-
     let ast = parse(this.get('code'));
-    return glimmer.buildAST(ast);
-
+    return etr.buildAST(ast);
   }),
 
-  nodeApi: computed('pseudoAst', function() {
-        return this.get('pseudoAst').join('\n//-----------------------\n');
+  nodeApi: computed('code', function() {
+    let ast = parse(this.get('code'));
+        return etr.buildAST(ast).join('\n//-----------------------\n');
   }),
 
-  output: computed('pseudoAst', function() {
+  output: computed('code', function() {
     const sampleCode = '';
     const outputAst = parse(sampleCode);  
 
     // Check the manifested api is working fine
-    this.get('pseudoAst').forEach(n => outputAst.body.push(eval(n)));
+    let ast = parse(this.get('code'));
+    etr.buildAST(ast).forEach(n => { 
+      outputAst.body.push(eval(n));
+    });
 
     const output = print(outputAst, { quote: 'single'});
 
--- app/utils/template-recast-builders.js
+++ app/utils/template-recast-builders.js
@@ -1,12 +1,18 @@
 function textNode(node) {
-  return `b.text('${node.chars.replace('\n','')}')`;
+  // To escape newline characters inside template literals
+  let t = node.chars.replace(/\n/g,'\\n');
+  return `b.text("${t}")`;
 }
 
+// Build element attributes
 function buildAttributes(attrs) {
   let _attrs = attrs.map(a => {
     switch(a.value.type) {
       case 'TextNode':
-        return `b.attr('${a.name}', b.text('${a.value.chars}'))`; 
+        return `b.attr('${a.name}', ${textNode(a.value)})`; 
+
+      case 'MustacheStatement':
+        return `b.attr('${a.name}', ${mustacheStatement(a.value)})`;
 
       default:
         console.log('buildAttributes => ', a.value.type); // eslint-disable-line
@@ -17,8 +23,78 @@ function buildAttributes(attrs) {
   return _attrs.join(',');
 }
 
+// Extracted from ember-angle-brackets-codemod transform
+function transformNestedSubExpression(subExpression) {
+  let positionalArgs = subExpression.params.map(param => {
+    if (param.type === 'SubExpression') {
+      return transformNestedSubExpression(param);
+    } else if (param.type === 'StringLiteral') {
+      return `"${param.original}"`;
+    } else {
+      return param.original;
+    }
+  });
+
+  let namedArgs = [];
+  if (subExpression.hash.pairs.length > 0) {
+    namedArgs = subExpression.hash.pairs.map(pair => {
+      if (pair.value.type === 'SubExpression') {
+        let nestedValue = transformNestedSubExpression(pair.value);
+        return `${pair.key}=${nestedValue}`;
+      } else {
+        if (pair.value.type === 'StringLiteral') {
+          return `${pair.key}="${pair.value.original}"`;
+        }
+        return `${pair.key}=${pair.value.original}`;
+      }
+    });
+  }
+
+  let args = positionalArgs.concat(namedArgs);
+  return `(${subExpression.path.original} ${args.join(' ')})`;
+}
+
+// Build Params for Mustache Statements
+// Extracted from ember-angle-brackets-codemod transform
+function buildParams(params) {
+  return params
+    .map(p => {
+      switch(p.type) {
+        case 'SubExpression': 
+          return transformNestedSubExpression(p);
+
+        case 'StringLiteral':
+          return `"${p.original}"`;
+
+        case 'NullLiteral':
+          return 'null';
+
+        case 'UndefinedLiteral':
+          return 'undefined';
+
+        default:
+          console.log('buildParams => ', p.type); // eslint-disable-line
+          return p.original;
+      }
+    })
+    .join(' ');
+
+
+}
+
+// Build Params for Block Statements
+function buildBlockParams(params) {
+  return params.map(p => {
+    return `b.path('${p.original}')`;
+  }).join(',');
+}
+
 function mustacheStatement(node) {
+  if(node.params.length > 0) {
+  return `b.mustache(b.path('${node.path.original} ${buildParams(node.params)}'))`;
+  } else {
   return `b.mustache('${node.path.original}')`;
+  }
 }
 function buildChildren(children) {
   return children.map(child => {
@@ -41,7 +117,7 @@ function buildChildren(children) {
   }).join(',');
 }
 function elementNode(node) {
-  let { selfClosing, tag, attributes, blockParams, children } = node;
+  let { selfClosing, tag, attributes, children } = node;
   return `b.element({name: '${tag}', selfClosing: ${selfClosing}},
     {
     attrs: [${buildAttributes(attributes)}],
@@ -50,26 +126,39 @@ function elementNode(node) {
   )`;
 }
 
+function blockStatement(node) {
+  let { path, program, params } = node;
+  return `b.program([
+      b.block(
+        b.path('${path.original}'),
+        [${buildBlockParams(params)}],
+        b.hash([b.path('lskdf'),'laskjdf']),
+        b.blockItself([${buildChildren(program.body)}])
+      ),
+    ])`;
+}
 function buildAST(ast) {
 
-// Build the Glimmer template api 
-    let _ast = ast.body.map(node => {
+  // Build the Glimmer template api 
+  let _ast = ast.body.map(node => {
 
-      switch(node.type) {
-        case 'TextNode':
-          return textNode(node);
+    switch(node.type) {
+      case 'TextNode':
+        return textNode(node);
 
-        case 'ElementNode':
-          return elementNode(node);
+      case 'ElementNode':
+        return elementNode(node);
 
-        default:
-          console.log('buildAST => ', node.type); // eslint-disable-line
-          return '';
-      }
+      case 'BlockStatement':
+        return blockStatement(node);
 
-    });
+      default:
+        console.log('buildAST => ', node.type); // eslint-disable-line
+    }
 
-    return _ast;
+  });
+
+  return _ast;
 
 }
 
