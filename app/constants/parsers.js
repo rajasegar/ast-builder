eade63ee573ca82d1d661b81edd60c42a44599ef
Rajasegar Chandran <rajasegar.c@gmail.com>
2020-01-06 05:37:54 +0530

    [CHORE] Removing unused parsers from the list

--- app/constants/parsers.js
+++ app/constants/parsers.js
@@ -11,15 +11,7 @@ export default {
     'babylon': {
       name: 'babylon',
       version: '7.6.4'
-    },
-    'flow': {
-      name: 'flow',
-      version: '0.112.0'
-    },
-    'typescript': {
-      name: 'typescript',
-      version: '3.6.4'
-    },
+    }
   },
   'Handlebars': {
     'ember-template-recast': {
