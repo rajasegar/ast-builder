e720248f9198b3f9465f4075b10b62cee01260e9
Rajasegar Chandran <rajasegar.c@gmail.com>
2021-06-10 07:54:31 +0530

    v3.14.0...v3.26.1

--- config/environment.js
+++ config/environment.js
@@ -1,7 +1,6 @@
 'use strict';
-const pkg = require('../package.json');
 
-module.exports = function(environment) {
+module.exports = function (environment) {
   let ENV = {
     modulePrefix: 'ast-builder',
     environment,
@@ -14,15 +13,14 @@ module.exports = function(environment) {
       },
       EXTEND_PROTOTYPES: {
         // Prevent Ember Data from overriding Date.parse.
-        Date: false
-      }
+        Date: false,
+      },
     },
 
     APP: {
       // Here you can pass flags/options to your application instance
       // when it is created
     },
-    pkg
   };
 
   if (environment === 'development') {
